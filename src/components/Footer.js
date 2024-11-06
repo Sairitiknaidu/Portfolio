23c718c2023671eb86dabc72be47b8c4c762b792
Sairitik Naidu <72565028+Sairitiknaidu@users.noreply.github.com>
2024-11-06 12:28:52 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Sairitik Naidu</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {2024} SR</h3>
+          <h3>Copyright © {year} SR</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
